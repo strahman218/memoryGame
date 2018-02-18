ee2b0b0fbca7211470b8f15095f7dde2dd91eaa1
strahman218 <strahman@wpi.edu>
2018-02-18 05:07:20 -0500

    part of player vs. computer working

--- app/game/game.js
+++ app/game/game.js
@@ -10,76 +10,86 @@ angular.module('myApp.game', ['ngRoute'])
 }]);
 
 function gameController($scope, $timeout){
-  var cardSuits = ['heart', 'spade', 'club', 'diamond'];
-  var colMax = 4;
-  var rowMax = 13;
-  var numCards = colMax * rowMax;
-  $scope.selectedCards = [];
+	var cardSuits = ['heart', 'spade', 'club', 'diamond'];
+	var colMax = 13;
+	var rowMax = 4;
+	var numCards = colMax * rowMax;
+	$scope.isGameOver = false;
+	$scope.selectedCards = [];
 
-  var cards = {};
-  $scope.board = [];
-  $scope.matches = [];
-  $scope.numMatches = 0;
-  $scope.expandMatches = false;
-  $scope.showGameEnd = false;
-  $scope.message = '';
+	var cards = {};
+	$scope.board = [];
+	$scope.message = '';
+	var totalMatches = 0;
+	
+	initializeGame();
+	$scope.currentPlayer = $scope.p1;
 
-  initializeGame();
+	var timeout;
+	var timerStarted = false;
+	
 
-  $scope.toggleCard = function(card){
-    card.show = !card.show;
+  $scope.toggleCard = function(card){	
+  if(timerStarted){
+	return;
+  }
+	if($scope.selectedCards.length < 2){
+		card.show = $scope.selectedCards.length <= 2;
+		if(!containsCard(card)){
+			$scope.selectedCards.push(card);
+		}
+	} 
 
-    //make sure you can't click on the same card twice, duh
-    if(!containsCard(card)){
-      $scope.selectedCards.push(card);
-    }
+	if ($scope.selectedCards.length == 2){
+		var card1 = $scope.selectedCards[0];
+		var card2 = $scope.selectedCards[1];
+		var isMatch = card1.value == card2.value;
+		
+		if(isMatch){ 
+			totalMatches++;
+			$scope.isGameOver = totalMatches == numCards/2;
 
-    if($scope.selectedCards.length == 2){
-      $scope.message = "you've already selected 2 cards!";
-      checkForMatch($scope.selectedCards[0], $scope.selectedCards[1])
-    }
+			$scope.message = $scope.isGameOver ? "GAME OVER" : "You found a match!";
+			timerStarted = true;
+			timeout = $timeout(function(){ setMatched(card1, card2); }, 1000)
+		} else {
+			timerStarted = true;
+		   $scope.message = "Not today! try again!";
+			timeout = $timeout(function(){ resetCards(card1, card2)}, 1000);
+		}
+	}
+	
+	$scope.currentPlayer.turn++;
   };
-
-  var checkForMatch = function(card1, card2){
-    if(card1 == card2){
-      $scope.message = "Pick a different card kid";
-    }else if(card1.value == card2.value ){
-      $scope.message = "You found a match! Congrats!";
-
-      //set the cards as matched
-      //show the display value
-      card1.matched = !card1.matched;
-      card2.matched = !card2.matched;
+  
+  var setMatched = function(card1, card2){
+      card1.matched = true;
+      card2.matched = true;
       card1.show = true;
       card2.show = true;
 
-      $scope.matches[$scope.numMatches] = [card1, card2];
-      $scope.numMatches += 1;
-      //clear the selected cards
+      $scope.currentPlayer.matches.push([card1, card2]);
       $scope.selectedCards = [];
-      $scope.expandMatches = true;
-      $timeout(function(){ $scope.message = "" }, 1000);
-
-      //if you won the game dawg
-      if($scope.numMatches == numCards/2){
-        $scope.showGameEnd = true;
-      }
-    } else {
-      //if the cards don't match, reset them
-      $scope.message = "Not today! try again!";
-      $timeout(function(){ resetCards(card1, card2)}, 1000);
-    }
-  };
+	  if(!$scope.isGameOver){resetTurn();}
+  }
 
   var resetCards = function(card1, card2){
-    card1.show = !card1.show;
-    card2.show = !card2.show;
+    console.log("resetCards()");
+    card1.show = false;
+    card2.show = false;
     $scope.selectedCards = [];
-    $scope.message = "";
+    resetTurn();
+
+  }
+  
+  var resetTurn = function(){
+	timerStarted = false;
+	$scope.currentPlayer = $scope.currentPlayer == $scope.p1 ? $scope.p2 : $scope.p1;
+	$scope.message = "";
   }
 
 
-  function containsCard(card){
+  var containsCard = function(card){
     for(var i=0; i<$scope.selectedCards.length; i++){
       if($scope.selectedCards[i] == card){
         return true
@@ -88,18 +98,45 @@ function gameController($scope, $timeout){
     return false;
   }
 
+  	$scope.$watch('currentPlayer', function(val){
+		if(val.name == 'Computer'){
+			computerTurn();
+			$timeout(function(){ computerTurn(); }, 1000);
+		}
+	})
+	
+  function computerTurn(){
+	//select 2 cards at random. maybe add something smart later
+	var randId = getRandomId();
+	while(cards[randId].matched || cards[randId].show){
+		randId = getRandomId();
+	}
+	 var card = cards[randId];
+	 console.log("computer turn");
+	 console.dir(card);
+	 $scope.toggleCard(card);
+  }
   //all the functions to initialize the game
+
   function initializeGame(){
+  console.log("initializeGame()");
     populateCardValues();
     createBoard();
+	addPlayers();
+	
+  }  
+  
+  function addPlayers(){
+	  $scope.p1 = new Player('You');
+	  $scope.p2 = new Player('Computer');
   }
-
+  
   function populateCardValues(){
+  console.log("populateCardValues()");
     for(var suit=0; suit<cardSuits.length; suit++){
-      for(var value=1; value<rowMax+1; value++) {
+      for(var value=1; value<14; value++) {
         var cardValue = [1, 11, 12, 13].indexOf(value) != -1 ? getCardValue(value) : value;
-        var cardId = getRandomValue();
-
+        var cardId = getRandomValue();		
         var tmp = new Card(cardId, cardValue, value, cardSuits[suit]);
         cards[cardId] = tmp;
       }
@@ -128,20 +165,23 @@ function gameController($scope, $timeout){
   }
 
   function createBoard(){
-
+  var row;
+	console.log("create board");
     //sets up the board
-    for(var row=0; row<rowMax; row++){
-      if(!$scope.board[row]){
+    for(row=0; row<rowMax; row++){
+      if($scope.board[row] == null){
         $scope.board[row] = [];
       }
     }
+	
     populateBoard();
   }
 
   function populateBoard(){
+  console.log("populateBoard()");
     var bRow = 0;
 
-    for(var card=0; card<numCards; card++){
+    for(var card=0; card<52; card++){
       if($scope.board[bRow].length < colMax){
         $scope.board[bRow].push(cards[card]);
       } else {
@@ -149,6 +189,9 @@ function gameController($scope, $timeout){
         $scope.board[bRow].push(cards[card]);
       }
     }
+	
+	console.log("board is done");
+	console.dir($scope.board);
   }
 
   function Card(id, display, value, suit){
@@ -159,9 +202,19 @@ function gameController($scope, $timeout){
     this.show = false;
     this.matched = false;
   }
+  
+  function Player(name){
+	this.name = name;
+	this.turn = 0;
+	this.matches = [];
+  }
 
   function getRandomValue(){
-    var rand = Math.floor(Math.random() * numCards);
-    return cards[rand] != null ? getRandomValue() : rand;
+    var rand = getRandomId();
+	return cards[rand] != null ? getRandomValue() : rand;
+  }
+  
+  function getRandomId(){
+	return Math.floor(Math.random() * numCards);
   }
 }
